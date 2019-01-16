ede52ca96520d5adf8ea2eaa3102116f71f16e5b
JoshuaMathias <joshuaamathias@gmail.com>
2019-01-16 15:10:50 -0800

    Setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'easyinfo',
+    packages = ['easyinfo'],
+    version = '0.0.0',  # Ideally should be same as your GitHub release tag varsion
+    description = 'Easy debug printing, timing, and variable attributes (length, width) for Python.',
+    author = 'Joshua Mathias',
+    author_email = 'joshuaamathias@gmail.com',
+    url = 'https://github.com/JoshuaMathias/easyinfo',
+    # download_url = 'download link you saved',
+    keywords = ['info', 'time', 'print', 'length'],
+    # classifiers = [],
+)
