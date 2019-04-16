5a2706c81ce1695b5ae66b3efddf7ae1b9c95032
Josh <joshuaamathias@gmail.com>
2019-04-16 16:00:40 -0700

    Handle Python 2
    import print from Python 3

--- utils.py
+++ utils.py
@@ -3,6 +3,8 @@
 Easy debug printing, timing, and variable attributes (length, width) for Python.
 
 """
+# if (sys.version_info < (3, 0)):
+from __future__ import print_function
 import sys
 import inspect
 import time
@@ -27,7 +29,7 @@ def prev_frame(num_back=1):
 # arg_i is the index of the argument in the function being called num_back frames ago
 #  if arg_i is -1, the name receiving variable of the receiving variable is returned
 #  e.g. var_name = vname(arg_i=-1) => returns 'var_name'
-def vname(var=None, num_back=2, func_name='vname', arg_i=0):
+def vname(var=None, num_back=2, func_name='vname', arg_i=0, arg_name=None):
   code = inspect.getframeinfo(prev_frame(num_back))[3][0]
   var_name = ''
   if arg_i < 0:
@@ -35,15 +37,21 @@ def vname(var=None, num_back=2, func_name='vname', arg_i=0):
     var_search = re.search(receiving_pattern, code)
     if var_search:
       var_name = var_search.group(1)
-      if not var_name:
-        var_name = ''
-      else:
-        var_name = var_name.replace("self.", "") # For loading class variables
   else:
     args_start = code.find(func_name+'(') + len(func_name) + 1 # +1 for (
     args_end = len(code) - code[::-1].find(')') - 1 # Search from the end, in case there's another function call inside
     args_str = code[args_start:args_end]
-    var_name = args_str.split(",")[arg_i].strip()
+    if arg_name:
+      named_pattern = func_name+r'\s*=\s*(\p{L}\w+)'
+      var_search = re.search(named_pattern, args_str)
+      if var_search:
+        var_name = var_search.group(1)
+    else:
+      var_name = args_str.split(",")[arg_i].strip()
+  if not var_name:
+    var_name = ''
+  else:
+    var_name = var_name.replace("self.", "") # For loading class variables
   return var_name
 
 def vline(num_back=2): # Credit to http://code.activestate.com/recipes/145297-grabbing-the-current-line-number-easily/
@@ -54,20 +62,23 @@ def vline(num_back=2): # Credit to http://code.activestate.com/recipes/145297-gr
 def eprint(*args, **kwargs):
   print(args[0], file=sys.stderr, **kwargs)
 
-def vstr(var, name=None, val=None, func_name='vstr', num_back=3):
+def vstr(var, name=None, val=None, func_name='vstr', num_back=3, verbose=None):
   if not val:
     val = var
   msg = None
   if not name:
     name = vname(var, num_back, func_name)
-  msg = name + " (line " + str(vline(num_back)) + ") " + str(type(var)) + ": " + str(val)
+  msg = name
+  if verbose is not False:
+    msg += " (line " + str(vline(num_back)) + ") " + str(type(var))
+  msg += ": " + str(val)
   return msg
   
-def vprint(var, name=None, val=None, **kwargs):
-  msg = vstr(var, name, val, func_name='vprint', num_back=4)
+def vprint(var, name=None, val=None, verbose=None, **kwargs):
+  msg = vstr(var, name, val, func_name='vprint', num_back=4, verbose=verbose)
   print(msg, **kwargs)
   
-def lstr(var, name=None, val=None, max_depth=10, func_name='lstr', num_back=3):
+def lstr(var, name=None, val=None, max_depth=10, func_name='lstr', num_back=3, verbose=None):
   attr_name = 'len'
   if not val:
     if hasattr(var, '__len__') and not hasattr(var, 'shape'):
@@ -103,22 +114,26 @@ def lstr(var, name=None, val=None, max_depth=10, func_name='lstr', num_back=3):
       val = var
   if not name:
     name = vname(var, num_back, func_name)
-  msg = name + " (line " + str(vline(num_back)) + ") " + attr_name + ": " + str(val)
+  msg = name
+  if verbose is not False:
+    msg += " (line " + str(vline(num_back)) + ") " + str(type(var)) + " " + attr_name
+  msg += ": " + str(val)
   return msg
   
 # Print len
-def lprint(var, name=None, val=None, num_back=4, **kwargs):
-  msg = lstr(var, name, val, func_name='lprint', num_back=4)
+def lprint(var, name=None, val=None, num_back=4, verbose=None, **kwargs):
+  msg = lstr(var, name, val, func_name='lprint', num_back=4, verbose=verbose)
   print(msg, **kwargs)
 
 # Get str of all printing functions output
-def astr(var, name=None, val=None, func_name='astr', num_back=4):
-  msg = lstr(var, name, val, func_name=func_name, num_back=num_back) + "\n" + vstr(var, "\t", val, func_name=func_name, num_back=num_back)
+def astr(var, name=None, val=None, func_name='astr', num_back=4, verbose=None):
+  msg = lstr(var, name, val, func_name=func_name, num_back=num_back, verbose=verbose) + "\n" + \
+    vstr(var, "\t", val, func_name=func_name, num_back=num_back, verbose=verbose)
   return msg
 
 # Call all printing functions for variable
-def aprint(var, name=None, val=None, **kwargs):
-  msg = astr(var, name, val, func_name='aprint', num_back=5)
+def aprint(var, name=None, val=None, verbose=None, **kwargs):
+  msg = astr(var, name, val, func_name='aprint', num_back=5, verbose=verbose)
   print(msg, **kwargs)
 
 def vlen(var):
@@ -150,6 +165,8 @@ def vwid(var):
       val = var.shape
       if len(val) > 1:
         val = val[1]
+      else:
+        val = 1
     elif hasattr(var, '__len__'):
       if len(var) > 0:
         val = vlen(var[0])
@@ -168,6 +185,12 @@ def add_file_suffix(filename, suf):
   name, ext = os.path.splitext(filename)
   return name+"_"+str(suf)+ext
 
+# Turn 'path/filename.txt' into 'path/pref_filename.txt'
+def add_file_prefix(filename, pre):
+  dirname = os.path.dirname(filename)
+  basename = os.path.basename(filename)
+  return os.path.join(dirname, str(pre)+"_"+basename)
+
 # Use pickle to save an object
 # If filepath include a filename at end, use that filename
 # If filepath is a directory, save object as filepath/variable_name.pkl
@@ -179,9 +202,11 @@ def add_file_suffix(filename, suf):
 #   using the variable name as file basename
 global _save_dir
 _save_dir = ''
-def vsave(obj, filepath=None, verbose=True, sort=True, save_dir=None):
+def vsave(obj, filepath=None, sort=True, save_dir=None, verbose=True,):
   global _save_dir
   if save_dir:
+    if filepath:
+      filepath = os.path.join(save_dir, filepath)
     _save_dir = save_dir
   var_name = vname(obj, num_back=3, func_name='vsave')
   ext = None
@@ -242,7 +267,7 @@ def vsave(obj, filepath=None, verbose=True, sort=True, save_dir=None):
 #   use receiving variable name as filename (e.g. 'test_var.pkl' for test_var = vload())
 # Also, for a filepath with no extension, use it as the directory
 # load_dir can be specified if different than _save_dir
-def vload(filepath=float('inf'), verbose=True, load_dir=None):
+def vload(filepath=float('inf'), load_dir=None, verbose=True):
   ext = ''
   if not load_dir:
     global _save_dir
@@ -285,7 +310,12 @@ def vload(filepath=float('inf'), verbose=True, load_dir=None):
     with open(filepath, 'rb') as bin_file:
       loaded_var = pickle.load(bin_file)
   if verbose:
-    lprint(loaded_var, "Loaded variable from "+filepath)
+    filepath_str = filepath
+    if len(filepath_str) > 50:
+      filepath_str = '.../'
+      # TODO: Include the last directory also
+      filepath_str += os.path.basename(filepath)
+    lprint(loaded_var, "Loaded variable from "+filepath_str)
   return loaded_var
 
 _start_time = time.time()
@@ -332,11 +362,49 @@ def end(msg=None):
 
 # Return an int, removing any non-digit chars other than . or -
 def to_int(text):
-  text_digits = ""
-  for c in text:
-    if c.isdigit() or c == '.' or c == '-':
-      text_digits += c
-  if text_digits:
-    return int(text_digits)
-  else:
-    return False
+  if isinstance(text, int):
+    return text
+  try:
+    text = int(text)
+    return text
+  except ValueError:
+    text = str(text)
+    text_digits = ""
+    for c in text:
+      if c.isdigit() or c == '.' or c == '-':
+        text_digits += c
+    if text_digits:
+      try:
+        return int(text_digits)
+      except ValueError:
+        return False
+    else:
+      return False
+
+# Return int if possible or float otherwise.
+def to_num(text):
+  if isinstance(text, int):
+    return text
+  try:
+    text_float = float(text)
+    text_int = int(text)
+    if text_int == text_float:
+      return text_int
+    return text_float
+  except ValueError:
+    text = str(text)
+    text_digits = ""
+    for c in text:
+      if c.isdigit() or c == '.' or c == '-':
+        text_digits += c
+    if text_digits:
+      try:
+        return int(text_digits)
+      except ValueError:
+        try:
+          score = float(score)
+        except ValueError:
+          pass
+        return False
+    else:
+      return False
